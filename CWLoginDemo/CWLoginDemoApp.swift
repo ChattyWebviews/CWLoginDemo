fb8b848b03b826c26cd0c56125c02eef28688091
Teodor Dermendjiev <tdermendjievft@gmail.com>
2023-07-14 11:32:38 +0300

    Initial Commit

---
+++ CWLoginDemo/CWLoginDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CWLoginDemoApp.swift
+//  CWLoginDemo
+//
+//  Created by Teodor Dermendzhiev on 14.07.23.
+//
+
+import SwiftUI
+
+@main
+struct CWLoginDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
